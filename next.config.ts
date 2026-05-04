5bca5e119fb10c4d114de7be554c608caf35d6b8
shindekalpesharun <shindekalpesharun@gmail.com>
2026-05-04 10:35:07 +0530

    mod: change portfolio

--- next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  allowedDevOrigins: ['9799-2402-e280-3e1d-1c4-4a4d-f588-d82f-2aef.ngrok-free.app'],
-};
-
-export default nextConfig;
